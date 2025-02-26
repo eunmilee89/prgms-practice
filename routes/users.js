2aedf7e827f228afb96aabc3b04385b24d35ddc2
Eunmi Lee <eunoia0235@gmail.com>
2025-02-27 04:46:38 +0900

    feat(users): mariadb연동 및 코드 수정

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../mariadb');
 
 router.use(express.json());
 
-let db = new Map();
-
-// 로그인 : POST /login
 router.post('/login', (req, res) => {
-  const { userId, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!userId || !password) {
-    return res.status(400).json({ message: '입력 값을 다시 확인해주세요.' });
-  }
+  const sql = 'SELECT * FROM users WHERE email =?';
+  conn.query(sql, email, (err, results) => {
+    let loginUser = results[0];
 
-  let foundUser;
-
-  db.forEach((user) => {
-    if (user.userId === userId) {
-      foundUser = user;
+    if (loginUser && loginUser.password === password) {
+      res.status(200).json({ message: `${loginUser.name}님 환영합니다.` });
+    } else {
+      res.status(404).json({
+        message: '이메일 또는 비밀번호가 틀렸습니다.',
+      });
     }
   });
-
-  if (!foundUser) {
-    return res.status(404).json({ message: '존재하지 않는 사용자입니다.' });
-  }
-
-  if (foundUser.password !== password) {
-    return res.status(400).json({ message: '비밀번호가 틀렸습니다.' });
-  }
-
-  return res.status(200).json({ message: `${foundUser.name}님 환영합니다.` });
 });
 
-// 회원 가입 : POST /join
 router.post('/join', (req, res) => {
-  const { userId, password, name } = req.body;
-
-  if (userId || password || name) {
-    db.set(userId, req.body);
-    res.status(201).json({
-      message: `${db.get(userId).name}님 환영합니다.`,
-    });
-  } else {
+  if (req.body == {}) {
     res.status(400).json({
       message: `입력 값을 다시 확인해주세요.`,
+    });
+  } else {
+    const { email, name, password, contact } = req.body;
+
+    const sql =
+      'INSERT INTO users (email, name, password, contact) VALUES(?, ?, ?, ?)';
+    const values = [email, name, password, contact];
+    conn.query(sql, values, (err, results) => {
+      res.status(201).json(results);
     });
   }
 });
@@ -51,36 +41,20 @@ router.post('/join', (req, res) => {
 router
   .route('/users')
   .get((req, res) => {
-    // 회원 개별 조회 : GET /users/:id
-    const { userId } = req.body;
+    const { email } = req.body;
 
-    const user = db.get(userId);
-    if (user) {
-      res.status(200).json({
-        userId: user.userId,
-        name: user.name,
-      });
-    } else {
-      res.status(404).json({
-        message: `존재하는 회원이 없습니다.`,
-      });
-    }
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    conn.query(sql, email, (err, results) => {
+      res.status(200).json(results);
+    });
   })
   .delete((req, res) => {
-    // 회원 개별 탈퇴 : DELETE /users/:id
-    const { userId } = req.body;
+    const { email } = req.body;
 
-    const user = db.get(userId);
-    if (user) {
-      db.delete(userId);
-      res.status(200).json({
-        message: `${user.name}님 다음에 또 뵙겠습니다.`,
-      });
-    } else {
-      res.status(404).json({
-        message: `존재하는 회원이 없습니다.`,
-      });
-    }
+    const sql = 'DELETE FROM users WHERE email = ?';
+    conn.query(sql, email, (err, results) => {
+      res.status(200).json(results);
+    });
   });
 
 module.exports = router;
