4a8add4e43c328452b959c81809b3a577ad78445
Eunmi Lee <eunoia0235@gmail.com>
2025-02-27 05:31:06 +0900

    feat(authors): mariadb연동 및 코드 수정

--- routes/authors.js
+++ routes/authors.js
@@ -1,66 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../mariadb');
 
 router.use(express.json());
-
-let db = new Map();
-let id = 1;
 
 router
   .route('/')
   .post((req, res) => {
-    if (req.body.author) {
-      let author = req.body;
-      db.set(id++, author);
-
-      res.status(201).json({
-        message: `${db.get(id - 1).author}님의 글쓰기 생활을 응원합니다.`,
+    const { name, userId } = req.body;
+    if (name && userId) {
+      const sql = 'INSERT INTO authors (name, user_id) VALUES(?, ?)';
+      const values = [name, userId];
+      conn.query(sql, values, (err, results) => {
+        res.status(201).json(results);
       });
     } else {
       res.status(400).json({
         message: '요청 값을 제대로 보내주세요',
       });
     }
-  }) // 작가 채널 개별 생성
+  })
   .get((req, res) => {
-    let { userId } = req.body;
-    let authors = [];
+    const { userId } = req.body;
 
-    if (db.size && userId) {
-      db.forEach((value) => {
-        if (value.userId === userId) {
-          authors.push(value);
+    const sql = 'SELECT * FROM authors WHERE user_id = ?';
+    if (userId) {
+      conn.query(sql, userId, (err, results) => {
+        if (results.length) {
+          res.status(200).json(results);
+        } else {
+          notFoundAuthor(res);
         }
       });
-
-      if (authors.length) {
-        res.status(200).json(authors);
-      } else {
-        notFoundAuthor();
-      }
     } else {
-      notFoundAuthor();
+      res.status(400).end();
     }
-  }); // 작가 채널 전체 조회
+  });
 
 router
   .route('/:id')
   .get((req, res) => {
     const id = parseInt(req.params.id);
 
-    let author = db.get(id);
-    if (author) {
-      res.status(200).json(author);
-    } else {
-      notFoundAuthor();
-    }
-  }) // 작가 채널 개별 조회
+    const sql = 'SELECT * FROM authors WHERE id = ?';
+    conn.query(sql, id, (err, results) => {
+      if (results.length) {
+        res.status(200).json(results);
+      } else {
+        notFoundAuthor(res);
+      }
+    });
+  })
   .put((req, res) => {
     const id = parseInt(req.params.id);
     let author = db.get(id);
 
     if (!author) {
-      notFoundAuthor();
+      notFoundAuthor(res);
     }
 
     let existingAuthor = author.author;
@@ -82,11 +78,11 @@ router
         message: `${author.author}(이)가 삭제되었습니다.`,
       });
     } else {
-      notFoundAuthor();
+      notFoundAuthor(res);
     }
   }); // 작가 채널 개별 삭제
 
-function notFoundAuthor() {
+function notFoundAuthor(res) {
   res.status(404).json({
     message: '작가 정보를 찾을 수 없습니다.',
   });
