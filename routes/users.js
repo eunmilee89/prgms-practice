ef4eabdc9f0064bb79f7f355a5e3e742bfab0f7e
Eunmi Lee <eunoia0235@gmail.com>
2025-02-28 12:53:38 +0900

    feat(users): express-validator 라이브러리를 사용해서 유효성 검사 추가

--- routes/users.js
+++ routes/users.js
@@ -1,60 +1,114 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../mariadb');
+const { body, param, validationResult } = require('express-validator');
 
 router.use(express.json());
 
-router.post('/login', (req, res) => {
-  const { email, password } = req.body;
+const validate = (req, res, next) => {
+  const err = validationResult(req);
 
-  const sql = 'SELECT * FROM users WHERE email =?';
-  conn.query(sql, email, (err, results) => {
-    let loginUser = results[0];
-
-    if (loginUser && loginUser.password === password) {
-      res.status(200).json({ message: `${loginUser.name}님 환영합니다.` });
-    } else {
-      res.status(404).json({
-        message: '이메일 또는 비밀번호가 틀렸습니다.',
-      });
-    }
-  });
-});
-
-router.post('/join', (req, res) => {
-  if (req.body == {}) {
-    res.status(400).json({
-      message: `입력 값을 다시 확인해주세요.`,
-    });
+  if (err.isEmpty()) {
+    return next();
   } else {
+    return res.status(400).json(err.array());
+  }
+};
+
+router.post(
+  '/login',
+  [
+    body('email').notEmpty().isEmail().withMessage('이메일 입력 필요'),
+    body('password').notEmpty().isString().withMessage('비밀번호 입력 필요'),
+    validate,
+  ],
+  (req, res) => {
+    const { email, password } = req.body;
+
+    const sql = 'SELECT * FROM users WHERE email =?';
+    conn.query(sql, email, (err, results) => {
+      if (err) {
+        return res.status(400).end();
+      }
+
+      let loginUser = results[0];
+
+      if (loginUser && loginUser.password === password) {
+        res.status(200).json({ message: `${loginUser.name}님 환영합니다.` });
+      } else {
+        res.status(404).json({
+          message: '이메일 또는 비밀번호가 틀렸습니다.',
+        });
+      }
+    });
+  }
+);
+
+router.post(
+  '/join',
+  [
+    body('email').notEmpty().isEmail().withMessage('이메일 입력 필요'),
+    body('password').notEmpty().isString().withMessage('비밀번호 입력 필요'),
+    body('name').notEmpty().isString().withMessage('이름 입력 필요'),
+    body('contact').notEmpty().isString().withMessage('연락처 입력 필요'),
+    validate,
+  ],
+  (req, res) => {
     const { email, name, password, contact } = req.body;
 
     const sql =
       'INSERT INTO users (email, name, password, contact) VALUES(?, ?, ?, ?)';
     const values = [email, name, password, contact];
     conn.query(sql, values, (err, results) => {
+      if (err) {
+        return res.status(400).end();
+      }
       res.status(201).json(results);
     });
   }
-});
+);
 
 router
   .route('/users')
-  .get((req, res) => {
-    const { email } = req.body;
+  .get(
+    [
+      body('email').notEmpty().isEmail().withMessage('이메일 입력 필요'),
+      validate,
+    ],
+    (req, res) => {
+      const { email } = req.body;
 
-    const sql = 'SELECT * FROM users WHERE email = ?';
-    conn.query(sql, email, (err, results) => {
-      res.status(200).json(results);
-    });
-  })
-  .delete((req, res) => {
-    const { email } = req.body;
+      const sql = 'SELECT * FROM users WHERE email = ?';
+      conn.query(sql, email, (err, results) => {
+        if (err) {
+          return res.status(400).end();
+        }
 
-    const sql = 'DELETE FROM users WHERE email = ?';
-    conn.query(sql, email, (err, results) => {
-      res.status(200).json(results);
-    });
-  });
+        res.status(200).json(results);
+      });
+    }
+  )
+  .delete(
+    [
+      body('email').notEmpty().isEmail().withMessage('이메일 입력 필요'),
+      validate,
+    ],
+    (req, res) => {
+      const { email } = req.body;
+
+      const sql = 'DELETE FROM users WHERE email = ?';
+      conn.query(sql, email, (err, results) => {
+        if (err) {
+          return res.status(400).end();
+        }
+
+        if (results.affectedRows === 0) {
+          res.status(400).end();
+        } else {
+          res.status(200).json(results);
+        }
+      });
+    }
+  );
 
 module.exports = router;
