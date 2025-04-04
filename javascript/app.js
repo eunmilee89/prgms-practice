5a6d39ebef00d5883107fd8e5c9788d0d340aab1
Eunmi Lee <eunoia0235@gmail.com>
2025-04-04 23:46:54 +0900

    chore: js 폴더로 파일 이동

--- javascript/app.js
+++ javascript/app.js
@@ -3,8 +3,8 @@ const app = express();
 
 app.listen(7777);
 
-const userRouter = require('./routes/users');
-const authorRouter = require('./routes/authors');
+const userRouter = require('../routes/users');
+const authorRouter = require('../routes/authors');
 
 app.use('/', userRouter);
 app.use('/authors', authorRouter);
