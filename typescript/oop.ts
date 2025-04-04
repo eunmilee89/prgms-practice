5e94dafb1653b6b8c2ab79b4c1211bbda50301cf
Eunmi Lee <eunoia0235@gmail.com>
2025-04-04 23:45:12 +0900

    feat: class로 객체 만들어보기

---
+++ typescript/oop.ts
@@ -0,0 +1,28 @@
+import { bindAll } from 'express-validator/lib/utils';
+
+// 일반적인 직원 정보
+class Employee {
+  constructor(
+    private _empName: string,
+    private _age: number,
+    private _empJob: string
+  ) {}
+
+  get empName() {
+    return this._empName;
+  }
+
+  set empName(val: string) {
+    this._empName = val;
+  }
+
+  printEmp = (): void => {
+    console.log(
+      `${this._empName}의 나이는 ${this._age}이고, 직업은 ${this._empJob}입니다.`
+    );
+  };
+}
+
+let employee1 = new Employee('kim', 20, 'developer');
+employee1.empName = 'lee';
+employee1.printEmp();
